edf096f6922db4666271dfa8724f2129f9a37856
ITAscend <ivan.totev@ascend.agency>
2025-04-28 21:34:31 +0300

    Adds comment

--- app/Actions/BitFinex/GetMidPrice.php
+++ app/Actions/BitFinex/GetMidPrice.php
@@ -4,6 +4,15 @@ namespace App\Actions\BitFinex;
 
 class GetMidPrice
 {
+    /**
+     * Calculate the mid price from bid and ask prices.
+     * This is abstracted as a separate class because from here the formatting could be changed
+     * which will affect the whole app.
+     *
+     * @param float $bid The bid price
+     * @param float $ask The ask price
+     * @return float The mid price
+     */
     public function execute(float $bid, float $ask): float
     {
         return ($ask + $bid) / 2;
